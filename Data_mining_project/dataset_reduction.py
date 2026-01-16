8fb477f7e22cca54db6d126fd07fa5dc1375782f
kilomo12 <stevendjock34@gmail.com>
2026-01-16 15:09:31 +0100

    bjr_1

--- Data_mining_project/dataset_reduction.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(
-    r"C:\Users\steve\Downloads\Data_Mining\Data_Mining_Project\Data_mining_project\dataset\DM1_game_dataset.csv"
-)
-
-df_ridotto = df.sample(n=1000, random_state=42)
-df_ridotto.to_csv("dataset_ridotto.csv", index=False)
-print("Dataset ridotto creato con successo e salvato come 'dataset_ridotto.csv'")
